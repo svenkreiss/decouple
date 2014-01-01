a55082dc1dd7ac6ac64d1696c9286fd099a8166a
Sven Kreiss <sk@svenkreiss.com>
2014-01-01 22:24:15 +0100

    Minor doc.

--- Decouple/src/effectiveModel.py
+++ Decouple/src/effectiveModel.py
@@ -644,7 +644,14 @@ class EffectiveParametrization:
 
 
 	def profileTwoNll( self, x=None, y=None, threshold=5000 ):
-		""" h is a dictionary of channel:muTmuW histograms. """
+		""" 
+		h is a dictionary of channel:muTmuW histograms. 
+
+		Minimization is done using L-BFGS-B here, but most minimization algorithms should be fine.
+		For more info: R. H. Byrd, P. Lu and J. Nocedal. A Limited Memory Algorithm for Bound 
+		Constrained Optimization, (1995), SIAM Journal on Scientific and 
+		Statistical Computing, 16, 5, pp. 1190-1208.
+		"""
 		if not self.profile or len(self.nuisanceParameters.keys())==0:
 			return self.evalTwoNll(x,y)
 
